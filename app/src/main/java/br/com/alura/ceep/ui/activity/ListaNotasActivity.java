456934a998829096df015299490716fd733e78e8
Alex Felipe <alexfelipevieira@gmail.com>
2021-01-21 12:41:19 -0300

    atualizando aula

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,8 +1,7 @@
 package br.com.alura.ceep.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ListView;
 
 import java.util.List;
